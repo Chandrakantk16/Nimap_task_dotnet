2d87dd56154a1ee17a345572aa15b8d2070f53ae
Chandrakant Kuswaha <chandrakantkuswaha58@gmail.com>
2024-12-31 19:52:20 +0530

    second commit

--- ProductDetailProject/Controllers/ProductController.cs
+++ ProductDetailProject/Controllers/ProductController.cs
@@ -1,21 +1,19 @@
 ﻿using ProductDetailProject.Models;
 using System.Data.Entity;
-using System.Data.Entity.Core.Common.CommandTrees.ExpressionBuilder;
 using System.Linq;
 using System.Web.Mvc;
 using PagedList;
+
 
 namespace ProductDetailProject.Controllers
 {
     public class ProductController : Controller
     {
-
         ServicesContext db = new ServicesContext();
+
         // GET: Product
-
-
-        //Pagination on server side for Product Table
-        public ActionResult Index(int ? page)
+        // Pagination on server side for Product Table
+        public ActionResult Index(int? page)
         {
             var pageNumber = page ?? 1;
             var pageSize = 10;
@@ -23,45 +21,61 @@ namespace ProductDetailProject.Controllers
             return View(pdata);
         }
 
-
         public ActionResult Create()
         {
             return View();
         }
 
-
         [HttpPost]
         public ActionResult Create(ProductTable pt)
         {
-            db.ProductTable.Add(pt);
-            db.SaveChanges();
-            return RedirectToAction("Index", "Product");
-        }
+            // Check for duplicate product
+            if (db.ProductTable.Any(p => p.ProductName == pt.ProductName))
+            {
+                ModelState.AddModelError("ProductName", "A product with the same name already exists.");
+                return View(pt);
+            }
 
+            if (ModelState.IsValid)
+            {
+                db.ProductTable.Add(pt);
+                db.SaveChanges();
+                return RedirectToAction("Index", "Product");
+            }
+
+            return View(pt);
+        }
 
         public ActionResult Edit(int id)
         {
-            var row = db.ProductTable.Where(model => model.ProductId == id).FirstOrDefault();
+            var row = db.ProductTable.FirstOrDefault(model => model.ProductId == id);
             return View(row);
         }
-
-
 
         [HttpPost]
         public ActionResult Edit(ProductTable pt)
         {
-            db.Entry(pt).State = EntityState.Modified;
-            db.SaveChanges();
-            return RedirectToAction("Index", "Product");
+            // Check for duplicate product (excluding the current product being edited)
+            if (db.ProductTable.Any(p => p.ProductName == pt.ProductName && p.ProductId != pt.ProductId))
+            {
+                ModelState.AddModelError("ProductName", "A product with the same name already exists.");
+                return View(pt);
+            }
 
+            if (ModelState.IsValid)
+            {
+                db.Entry(pt).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index", "Product");
+            }
+
+            return View(pt);
         }
-
 
         public ActionResult Delete()
         {
             return View();
         }
-
 
         [HttpPost]
         public ActionResult Delete(int id)
@@ -73,35 +87,30 @@ namespace ProductDetailProject.Controllers
                 {
                     context.ProductTable.Remove(data);
                     context.SaveChanges();
-                    return RedirectToAction("index", "Product");
+                    return RedirectToAction("Index", "Product");
                 }
                 else
+                {
                     return View();
+                }
             }
         }
 
-
-
-
-        //Pagination on server side for ProductList
+        // Pagination on server side for ProductList
         public ActionResult ProductList(int? page)
         {
             int pageNumber = page ?? 1;
             int pageSize = 10;
             var productlist = (from pt in db.ProductTable
-                                join ct in db.CategoryTable on pt.CategoryId equals ct.CategoryId
+                               join ct in db.CategoryTable on pt.CategoryId equals ct.CategoryId
                                select new ShowData
-                                {
-                                    ProductId = pt.ProductId,
-                                    ProductName = pt.ProductName,
-                                    CategoryId = ct.CategoryId,
-                                    CategoryName = ct.CategoryName
-                                }).OrderBy(pt => pt.CategoryId).ToPagedList(pageNumber, pageSize);
+                               {
+                                   ProductId = pt.ProductId,
+                                   ProductName = pt.ProductName,
+                                   CategoryId = ct.CategoryId,
+                                   CategoryName = ct.CategoryName
+                               }).OrderBy(pt => pt.CategoryId).ToPagedList(pageNumber, pageSize);
             return View(productlist);
-
-          
-
         }
     }
 }
-    
