89ede42c430d285e75cf025f739318080085b6a0
Chandrakant Kuswaha <chandrakantkuswaha58@gmail.com>
2024-12-31 20:09:16 +0530

    second commit

--- ProductDetailProject/Controllers/CategoryController.cs
+++ ProductDetailProject/Controllers/CategoryController.cs
@@ -4,17 +4,15 @@ using System.Data.Entity;
 using System.Linq;
 using System.Web.Mvc;
 
-
 namespace ProductDetailProject.Controllers
 {
     public class CategoryController : Controller
     {
-
         ServicesContext db = new ServicesContext();
-        // GET: Category
 
-        //Pagination on server side for Category Table
-        public ActionResult Index(int ? page)
+        // GET: Category
+        // Pagination on server side for Category Table
+        public ActionResult Index(int? page)
         {
             var pageNumber = page ?? 1;
             var pageSize = 5;
@@ -22,179 +20,70 @@ namespace ProductDetailProject.Controllers
             return View(cdata);
         }
 
-
         public ActionResult Insert()
         {
             return View();
         }
 
-        //to insert:-
+        // To insert
         [HttpPost]
         public ActionResult Insert(CategoryTable ct)
         {
-                db.CategoryTable.Add(ct);
-                db.SaveChanges();
-                return RedirectToAction("Index", "Category");
-              
-            
+            if (db.CategoryTable.Any(x => x.CategoryName == ct.CategoryName))
+            {
+                ModelState.AddModelError("CategoryName", "Category already exists.");
+                return View(ct); // Return the same view with error message
+            }
+
+            db.CategoryTable.Add(ct);
+            db.SaveChanges();
+            return RedirectToAction("Index", "Category");
         }
 
-
-        //to Edit:-
+        // To edit
         public ActionResult Edit(int id)
         {
             var row = db.CategoryTable.Where(model => model.CategoryId == id).FirstOrDefault();
             return View(row);
         }
 
-
-
         [HttpPost]
         public ActionResult Edit(CategoryTable ct)
         {
+            if (db.CategoryTable.Any(x => x.CategoryName == ct.CategoryName && x.CategoryId != ct.CategoryId))
+            {
+                ModelState.AddModelError("CategoryName", "Category already exists.");
+                return View(ct); // Return the same view with error message
+            }
+
             db.Entry(ct).State = EntityState.Modified;
             db.SaveChanges();
             return RedirectToAction("Index", "Category");
-
         }
 
-
-        //to Delete:-
+        // To delete
         public ActionResult Delete()
         {
             return View();
         }
 
-
         [HttpPost]
         public ActionResult Delete(int Id)
         {
-
-             using (var context = new ServicesContext())
+            using (var context = new ServicesContext())
             {
                 var _data = context.CategoryTable.FirstOrDefault(x => x.CategoryId == Id);
                 if (_data != null)
                 {
                     context.CategoryTable.Remove(_data);
                     context.SaveChanges();
-                    return RedirectToAction("index", "Category");
+                    return RedirectToAction("Index", "Category");
                 }
                 else
-                    return View();
-            }
-        }
-    }
-}
-
-
-***************************************************************************
-
-
-
-using ProductDetailProject.Models;
-using System.Data.Entity;
-using System.Data.Entity.Core.Common.CommandTrees.ExpressionBuilder;
-using System.Linq;
-using System.Web.Mvc;
-using PagedList;
-
-namespace ProductDetailProject.Controllers
-{
-    public class ProductController : Controller
-    {
-
-        ServicesContext db = new ServicesContext();
-        // GET: Product
-
-
-        //Pagination on server side for Product Table
-        public ActionResult Index(int ? page)
-        {
-            var pageNumber = page ?? 1;
-            var pageSize = 10;
-            var pdata = db.ProductTable.OrderBy(x => x.CategoryId).ToPagedList(pageNumber, pageSize);
-            return View(pdata);
-        }
-
-
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-
-        [HttpPost]
-        public ActionResult Create(ProductTable pt)
-        {
-            db.ProductTable.Add(pt);
-            db.SaveChanges();
-            return RedirectToAction("Index", "Product");
-        }
-
-
-        public ActionResult Edit(int id)
-        {
-            var row = db.ProductTable.Where(model => model.ProductId == id).FirstOrDefault();
-            return View(row);
-        }
-
-
-
-        [HttpPost]
-        public ActionResult Edit(ProductTable pt)
-        {
-            db.Entry(pt).State = EntityState.Modified;
-            db.SaveChanges();
-            return RedirectToAction("Index", "Product");
-
-        }
-
-
-        public ActionResult Delete()
-        {
-            return View();
-        }
-
-
-        [HttpPost]
-        public ActionResult Delete(int id)
-        {
-            using (var context = new ServicesContext())
-            {
-                var data = context.ProductTable.FirstOrDefault(x => x.ProductId == id);
-                if (data != null)
                 {
-                    context.ProductTable.Remove(data);
-                    context.SaveChanges();
-                    return RedirectToAction("index", "Product");
-                }
-                else
                     return View();
+                }
             }
-        }
-
-
-
-
-        //Pagination on server side for ProductList
-        public ActionResult ProductList(int? page)
-        {
-            int pageNumber = page ?? 1;
-            int pageSize = 10;
-            var productlist = (from pt in db.ProductTable
-                                join ct in db.CategoryTable on pt.CategoryId equals ct.CategoryId
-                               select new ShowData
-                                {
-                                    ProductId = pt.ProductId,
-                                    ProductName = pt.ProductName,
-                                    CategoryId = ct.CategoryId,
-                                    CategoryName = ct.CategoryName
-                                }).OrderBy(pt => pt.CategoryId).ToPagedList(pageNumber, pageSize);
-            return View(productlist);
-
-          
-
         }
     }
 }
-    
